68b52e7943d3b214e71ec6ba9b2f014bb665c8d1
karinaconstandache <karinaconstandache12@gmail.com>
2025-05-21 19:08:15 +0300

    fixed images and enhanced Taco Bell menu UI (client)

---
+++ TacoBell/Helpers/ImagePathConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace TacoBell.Helpers
+{
+    public class ImagePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not string path || string.IsNullOrWhiteSpace(path))
+                return null;
+
+            try
+            {
+                // Elimină slashul de început dacă e cazul
+                if (path.StartsWith("/"))
+                    path = path.Substring(1);
+
+                // Calea completă față de executabil
+                string fullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path);
+
+                if (!File.Exists(fullPath))
+                    return null;
+
+                return new BitmapImage(new Uri(fullPath, UriKind.Absolute));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
